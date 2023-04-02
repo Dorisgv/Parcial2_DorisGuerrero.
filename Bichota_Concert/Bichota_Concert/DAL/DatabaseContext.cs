41a567c26e4f7127c1b71d0939624f68811e223f
Guerrero Doris <guerrerodoris08@gmail.com>
2023-04-02 03:15:27 -0500

    Creation of CRUD and some changes.

--- Bichota_Concert/Bichota_Concert/DAL/DatabaseContext.cs
+++ Bichota_Concert/Bichota_Concert/DAL/DatabaseContext.cs
@@ -11,5 +11,11 @@ namespace Bichota_Concert.DAL
         }
 
         public DbSet<Ticket> Tickets { get; set; }
+        //Esto me sirve para hacer indensaciones para cada uno de los campos de mi BD's
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Ticket>().HasIndex(t => t.Name).IsUnique();
+        //}
     }
 }
--- Bichota_Concert/Bichota_Concert/DAL/Entities/Entity.cs
+++ Bichota_Concert/Bichota_Concert/DAL/Entities/Entity.cs
@@ -4,10 +4,11 @@ namespace Bichota_Concert.DAL.Entities
 {
     public class Entity
     {
-        [Key] //Esto son datanotations, debe ubicarse encima de la propiedad que va hacer mi PK.
-        [Required] //Este significa que el campo Id es requerido.
+        [Key] 
+        [Required] 
         public Guid Id { get; set; }
-        public DateTime? CreatedDate { get; set; } //el signo de pregunta quiere decir que esos datos pueden ser nulos.
+        public DateTime? CreatedDate { get; set; }
+        [Display(Name = "Fecha de Modificacion")]
         public DateTime? ModifiedDate { get; set; }
     }
 }
